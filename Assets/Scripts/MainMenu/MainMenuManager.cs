5b1b0cf9cbedf007a2ea1b875a00d61f6b274256
trflorian <florian.trautweiler@hispeed.ch>
2022-09-10 14:05:02 +0200

    hook up host and join game buttons to fishnet networking manager

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,44 @@
+using System;
+using FishNet;
+using FishNet.Managing;
+using FishNet.Managing.Scened;
+using FishNet.Transporting;
+using FishNet.Utility;
+using UnityEngine;
+
+namespace MainMenu
+{
+    /// <summary>
+    /// Manages network setup in main menu
+    /// </summary>
+    public class MainMenuManager : MonoBehaviour
+    {
+        private NetworkManager _networkManager;
+
+        private void Awake()
+        {
+            _networkManager = InstanceFinder.NetworkManager;
+        }
+
+        private void StartClient()
+        {
+            _networkManager.ClientManager.StartConnection();
+        }
+
+        private void StartServer()
+        {
+            _networkManager.ServerManager.StartConnection();
+        }
+
+        public void OnClickStartHost()
+        {
+            StartServer();
+            StartClient();
+        }
+
+        public void OnClickStartClient()
+        {
+            StartClient();
+        }
+    }
+}
