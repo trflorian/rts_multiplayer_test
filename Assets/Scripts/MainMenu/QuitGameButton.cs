70d7bd61baed94b168b11580c19c8d456340bc65
trflorian <florian.trautweiler@hispeed.ch>
2022-09-10 13:47:11 +0200

    main menu added buttons for hosting, joining and quitting

---
+++ Assets/Scripts/MainMenu/QuitGameButton.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    /// <summary>
+    /// Loads the game scene on click
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public class QuitGameButton : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var button = GetComponent<Button>();
+            button.onClick.AddListener(QuitGame);
+        }
+
+        private static void QuitGame()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
